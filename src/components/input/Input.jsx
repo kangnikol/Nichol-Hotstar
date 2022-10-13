f21452bdd0f7df129cd3a408eb8a4b9a81946a57
kangnikol <nicholasalvisaputra@gmail.com>
2022-10-13 10:44:24 +0700

    update

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,14 +1,17 @@
 import React from "react"
-import "./Input.scss"
+// import "./Input.scss"
 
 const Input = (props) => {
   return (
-    <input
-      type={props.type}
-      placeholder={props.placeholder}
-      value={props.value}
-      onChange={props.onChange ? (e) => props.onChange(e) : null}
-    />
+    <div className="text-center">
+      <input
+        className="border-0 bg-black px-6 py-3 rounded-full text-white"
+        type={props.type}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange ? (e) => props.onChange(e) : null}
+      />
+    </div>
   )
 }
 
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -10,7 +10,12 @@ const Modal = (props) => {
   }, [props.active])
 
   return (
-    <div id={props.id} className={`modal ${active ? "active" : ""}`}>
+    <div
+      id={props.id}
+      className={`modal flex items-center justify-center fixed z-[100] top-0 left-0 right-0 bottom-0 overflow-auto bg-black/40 opacity-0 invisible ${
+        active ? "opacity-1 visible" : ""
+      }`}
+    >
       {props.children}
     </div>
   )
@@ -28,7 +33,10 @@ export const ModalContent = (props) => {
     if (props.onClose) props.onClose()
   }
   return (
-    <div ref={contentRef} className="modal-content">
+    <div
+      ref={contentRef}
+      className="p-8 bg-gray-900 w-1/2 transform -translate-y-64 [transition:_transform_0.6s_ease,_opacity_0.6s_ease] relative"
+    >
       {props.children}
       <div className="modal-content-close" onClick={closeModal}>
         <i className="fa-solid fa-xmark"></i>
