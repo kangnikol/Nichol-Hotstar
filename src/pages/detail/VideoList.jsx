397fae4817e45bf12aa999322a0c097d4d9ae0ed
kangnikol <nicholasalvisaputra@gmail.com>
2023-06-12 14:19:04 +0700

    mfx

--- src/pages/detail/VideoList.jsx
+++ src/pages/detail/VideoList.jsx
@@ -9,8 +9,34 @@ const VideoList = (props) => {
     className: "text-white",
     infinite: false,
     autoplay: false,
-    slidesToShow: 5,
-    slidesToScroll: 5,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   }
 
   const { category } = useParams()
